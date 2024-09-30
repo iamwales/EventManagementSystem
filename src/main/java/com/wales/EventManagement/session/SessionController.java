5704f5228cf4c6a6e61b461686cc9499fa4df80c
iamwales01 <aolawale@360alumni.com>
2024-09-30 17:16:25 +0100

    added session controllers

--- src/main/java/com/wales/EventManagement/session/SessionController.java
+++ src/main/java/com/wales/EventManagement/session/SessionController.java
@@ -1,4 +1,27 @@
 package com.wales.EventManagement.session;
 
+import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Controller
 public class SessionController {
+    private final SessionRepository sessionRepository;
+
+    @QueryMapping
+    List<Session> sessions() {
+        return sessionRepository.findAll();
+    }
+
+    @QueryMapping
+    Optional<Session> session(@Argument UUID uuid) {
+        return sessionRepository.findById(uuid);
+    }
+
 }
